d6a6e62a29fb9bb4d6d01c7f7f8008b7c4be2b2b
n-hari159 <n.hari159@gmail.com>
2019-03-04 20:45:15 +0530

    new script file added

---
+++ portfolio-app/src/components/App6.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App6 extends Component {
+    render() {
+        return ( <
+            div >
+            <
+            a className = "twitter-timeline"
+            href = "https://twitter.com/NHari159?ref_src=twsrc%5Etfw" > < p > Tweets by NHari159 < /p></a >
+            <
+            /div>
+        );
+    }
+}
+
+export default App6;
