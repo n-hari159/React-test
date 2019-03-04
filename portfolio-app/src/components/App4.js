72601107be87c7b59e3d79807a19d1eb75f3b969
n-hari159 <n.hari159@gmail.com>
2019-03-04 20:44:47 +0530

    new script file added

---
+++ portfolio-app/src/components/App4.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class App4 extends Component {
+    render() {
+        return ( <
+            div >
+            <
+            h2 className = "w3-text-light-grey" > Contact Me < /h2> <
+            hr style = {
+                { width: '200px' }
+            }
+            className = "w3-opacity" / >
+            <
+            div className = "w3-section" >
+            <
+            p > < i className = "fa fa-map-marker fa-fw w3-text-white w3-xxlarge w3-margin-right" > < /i> Hyderabad, India </p >
+            <
+            p > < i className = "fa fa-phone fa-fw w3-text-white w3-xxlarge w3-margin-right" > < /i> Phone: +91 9164657955 </p >
+            <
+            p > < i className = "fa fa-envelope fa-fw w3-text-white w3-xxlarge w3-margin-right" > < /i> Email: n.hari159@gmail.com </p >
+            <
+            /div><br></br >
+            <
+            /div>
+        );
+    }
+}
+
+export default App4;
