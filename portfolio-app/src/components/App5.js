3ac1b6e60e3879f16c8e463c53a023de327abc6b
n-hari159 <n.hari159@gmail.com>
2019-03-04 20:44:59 +0530

    new script file added

---
+++ portfolio-app/src/components/App5.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class App5 extends Component {
+    render() {
+        return ( <
+            div >
+            <
+            footer className = "w3-content w3-padding-64 w3-text-grey w3-xlarge" >
+            <
+            a href = "https://www.facebook.com/harryn.159"
+            target = "_blank"
+            rel = 'noopener noreferrer' >
+            <
+            i className = "fa fa-facebook-official w3-hover-opacity" > < /i> < /
+            a > <
+            a href = "https://www.instagram.com/n.hari159/"
+            target = "_blank"
+            rel = 'noopener noreferrer' >
+            <
+            i className = "fa fa-instagram w3-hover-opacity" > < /i> < /
+            a > <
+            a href = "https://twitter.com/NHari159"
+            target = "_blank"
+            rel = 'noopener noreferrer' >
+            <
+            i className = "fa fa-twitter w3-hover-opacity" > < /i> < /
+            a > <
+            a href = "https://www.linkedin.com/in/hari-nakka-56b85973/"
+            target = "_blank"
+            rel = 'noopener noreferrer' >
+            <
+            i className = "fa fa-linkedin w3-hover-opacity" > < /i> < /
+            a > <
+            /footer> < /
+            div >
+        );
+    }
+}
+
+export default App5;
