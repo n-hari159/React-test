d63af31ca4a779f26ec032435e9d63693b099f3e
n-hari159 <n.hari159@gmail.com>
2019-03-04 20:45:52 +0530

    updated

--- portfolio-app/src/index.js
+++ portfolio-app/src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import App1 from './components/App1';
+import App2 from './components/App2';
+import App3 from './components/App3';
+import App4 from './components/App4';
+import App5 from './components/App5';
+import App6 from './components/App6';
 
 ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render( < App1 / > , document.getElementById('root1'));
+ReactDOM.render( < App2 / > , document.getElementById('root2'));
+ReactDOM.render( < App3 / > , document.getElementById('photos'));
+ReactDOM.render( < App4 / > , document.getElementById('contact'));
+ReactDOM.render( < App5 / > , document.getElementById('root3'));
+ReactDOM.render( < App6 / > , document.getElementById('root4'));
